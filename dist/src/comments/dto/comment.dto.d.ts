0e2cbc58d3252db34c4cbd759de4b1074dfb1ffd
MbuguaIT <jacobmbuguait@gmail.com>
2026-05-14 10:03:25 +0300

    REMOVE DIST FILES FROM GIT

--- dist/src/comments/dto/comment.dto.d.ts
+++
@@ -1,7 +0,0 @@
-import { CommentTargetType } from '../schemas/comment.schema';
-export declare class CreateCommentDto {
-    targetType: CommentTargetType;
-    targetId: string;
-    content: string;
-    mentions?: string[];
-}
--- dist/src/common/interceptors/logging.interceptor.d.ts
+++
@@ -1,9 +0,0 @@
-import { NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { LogsService } from '../../logs/logs.service';
-export declare class LoggingInterceptor implements NestInterceptor {
-    private readonly logsService;
-    constructor(logsService: LogsService);
-    intercept(context: ExecutionContext, next: CallHandler): Observable<any>;
-    private sanitizeData;
-}
